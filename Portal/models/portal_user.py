1be57d0668ca705bccfc05b982ece3b41f688196
Alexandre Cloquet <Alexandre.cloquet@gmail.com>
2015-06-05 10:23:15 +0200

    Add help text

--- Portal/models/portal_user.py
+++ Portal/models/portal_user.py
@@ -3,19 +3,29 @@ __author__ = 'Alexandre Cloquet'
 from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User
 from django.db import models
-from .enrollment import CharacterAttribute
+from .enrollment import CharacterAttribute, Game
+
 
 class Character(models.Model):
+    '''
+        Keep all characters from user for each game in database
+    '''
     user = models.ForeignKey(User)
+    game = models.ForeignKey(Game)
+    main_character = models.BooleanField(_("Main character"), default=False, help_text="You can specify if this character is your main")
+    level = models.SmallIntegerField(_('Character level'), blank=True, help_text="Specify your character level")
     roles = models.ManyToManyField(CharacterAttribute)
 
 
 class Absence(models.Model):
+    '''
+        Allows to save user absence in database
+    '''
     user = models.ForeignKey(User)
-    status = models.BooleanField(_('On holiday'), default=False)
-    from_date = models.DateField()
-    to_date = models.DateField()
-    reason = models.TextField(_('Reason'))
+    status = models.BooleanField(_('On holiday'), default=False, help_text="")
+    from_date = models.DateField(_('From'), help_text="Please use the following format: DD-MM-YYYY")
+    to_date = models.DateField(_('to'), help_text="Please use the following format: DD-MM-YYYY")
+    reason = models.TextField(_('Reason'), help_text="You can explain why you are absent")
 
 
 STATUS_USER = (
@@ -23,6 +33,7 @@ STATUS_USER = (
     (_("Inactive"), _("Inactive")),
     (_("Absent"), _("Absent")),
 )
+
 
 class Userprofile(models.Model):
     user = models.OneToOneField(User)
