3ae67ba91c2e00a5412b30b1e8696ffa69818a98
Jordan Kergoat <jordan.kergoat@gmail.com>
2015-06-08 15:53:29 +0200

    no message

--- GuildPortal/settings.py
+++ GuildPortal/settings.py
@@ -99,10 +99,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(os.getcwd(), 'media/')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+print(MEDIA_ROOT)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
--- GuildPortal/urls.py
+++ GuildPortal/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from Portal.views import index
 
 urlpatterns = patterns('',
@@ -14,4 +15,17 @@ urlpatterns = patterns('',
     url(r'', include('SuperPortal.urls')),
 )
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.conf import settings
+if settings.DEBUG:
+    urlpatterns += patterns('',
+
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+    url(r'^media/news/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
+)
