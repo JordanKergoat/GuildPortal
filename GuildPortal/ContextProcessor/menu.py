c0b973120842ba361a5442176d3f47f2ba0b71cd
Jordan Kergoat <jordan.kergoat@gmail.com>
2015-06-11 10:23:49 +0200

    no message

--- GuildPortal/ContextProcessor/menu.py
+++ GuildPortal/ContextProcessor/menu.py
@@ -1,7 +1,7 @@
 __author__ = 'Alexandre Cloquet'
 
 from Portal.models import Portal
-from SuperPortal.models import GuildSettings
+from SuperPortal.models import GuildSettings, SuperPortal
 
 def menu(request):
     return {
@@ -11,5 +11,6 @@ def menu(request):
 
 def guild_info(request):
     return {
-        'guild': GuildSettings.objects.all().first()
+        'guild': GuildSettings.objects.all().first(),
+        'super_portal': SuperPortal.objects.all().first()
     }
