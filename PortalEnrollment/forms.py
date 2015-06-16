c1d46ecaf22595bffc65c310101d65cde71e6071
Alexandre Cloquet <Alexandre.cloquet@gmail.com>
2015-06-16 16:34:36 +0200

    no message

--- PortalEnrollment/forms.py
+++ PortalEnrollment/forms.py
@@ -4,7 +4,18 @@ __author__ = 'Alexandre Cloquet'
 from django import forms
 from .models import Enrollement, EnrollmentSettings
 
-class EnrollementForm(forms.ModelForm):
+class OpenEnrollementForm(forms.ModelForm):
     class Meta:
         model = Enrollement
         exclude = ('user',)
+        widgets = {
+            'game_choice': forms.Select(attrs={
+                'onchange' : "charactersAPI(this.value, this.id)"
+            })
+        }
+
+
+class EnrollementForm(forms.ModelForm):
+    class Meta:
+        model = Enrollement
+        exclude = ('user', 'game_choice')
--- PortalEnrollment/urls.py
+++ PortalEnrollment/urls.py
@@ -3,10 +3,12 @@ __author__ = 'Alexandre Cloquet'
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from .views import EnrollementView as index
+from .views import OpenEnrollementView, CharacterAttributesView, EnrollementView
 from Portal.views import index as portal_index
 from Forum.views import index as forum_index
 
 urlpatterns = patterns('',
-    url(r'^$', index.as_view(), name='enrollement_index'),
+    url(r'^$', OpenEnrollementView.as_view(), name='enrollement_index'),
+    url(r'^(?P<id_application>(\d+))/$', EnrollementView.as_view(), name='enrollement_index'),
+    url(r'^character_attributes$', CharacterAttributesView.as_view(), name='character_attributes'),
                        )
