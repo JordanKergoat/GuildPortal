94e5d21b77c015e6433d8aedbec7ad15947eaf2b
Jordan Kergoat <jordan.kergoat@gmail.com>
2015-06-26 14:47:30 +0200

    no message

--- PortalEnrollment/urls.py
+++ PortalEnrollment/urls.py
@@ -2,28 +2,27 @@ from django.views.decorators.http import require_POST
 
 __author__ = 'Alexandre Cloquet'
 
-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from .views import OpenEnrollementView, CharacterAttributesView, EnrollementView, EnrollementListView, EnrollmentDetailView,\
+from .views import OpenEnrollementView, CharacterAttributesView, EnrollementView, EnrollementListView, \
+    EnrollmentDetailView, \
     CommentEnrollmentFormView, EnrollmentDetail, voteUp, voteDown, redirect_to_bnet, tmp
 from Portal.views import index as portal_index
 from Forum.views import index as forum_index
 
 urlpatterns = patterns('',
-    url(r'^$', OpenEnrollementView.as_view(), name='enrollement_index'),
-    url(r'^list_enrollement/$', EnrollementListView.as_view(), name='enrollement_list'),
+                       url(r'^$', OpenEnrollementView.as_view(), name='enrollement_index'),
+                       url(r'^list_enrollement/$', EnrollementListView.as_view(), name='enrollement_list'),
 
+                       url(r'^test/$', redirect_to_bnet, name='test_battlenet'),
+                       url(r'^tmp/$', tmp, name='test_battlenet'),
 
-    url(r'^test/$', redirect_to_bnet, name='test_battlenet'),
-    url(r'^tmp/$', tmp, name='test_battlenet'),
+                       url(r'^detail/(?P<pk>\d+)/$', EnrollmentDetail.as_view(), name='enrollment_detail'),
+                       url(r'^detail/(?P<pk>\d+)/comment/$', require_POST(CommentEnrollmentFormView.as_view()),
+                           name='enrollment_detail_comment'),
+                       url(r'^detail/(?P<pk>\d+)/voteup/$', voteUp, name='voteup'),
+                       url(r'^detail/(?P<pk>\d+)/votedown/$', voteDown, name='votedown'),
 
-    url(r'^detail/(?P<pk>\d+)/$', EnrollmentDetail.as_view(), name='enrollment_detail'),
-    url(r'^detail/(?P<pk>\d+)/comment/$', require_POST(CommentEnrollmentFormView.as_view()), name='enrollment_detail_comment'),
-    url(r'^detail/(?P<pk>\d+)/voteup/$', voteUp, name='voteup'),
-    url(r'^detail/(?P<pk>\d+)/votedown/$', voteDown, name='votedown'),
-
-
-    url(r'^(?P<id_application>(\d+))/$', EnrollementView.as_view(), name='enrollement'),
-    url(r'^character_attributes$', CharacterAttributesView.as_view(), name='character_attributes'),
+                       url(r'^(?P<id_application>(\d+))/$', EnrollementView.as_view(), name='enrollement_application'),
+                       url(r'^character_attributes$', CharacterAttributesView.as_view(), name='character_attributes'),
                        )
--- SuperPortal/views.py
+++ SuperPortal/views.py
@@ -15,7 +15,6 @@ def index(request):
     context['short_description'] = GuildSettings.objects.all().first().short_guild_description
     # context['list_portal'] = Portal.objects.all()
     context['news_list'] = News.objects.all().order_by('-published_date')
-    print(str(type(EnrollmentSettings.openned_enrollement)))
     context['enrollments'] = EnrollmentSettings.openned_enrollement.all()
 
     return render(request, "SuperPortal/index.html", context=context)
