b3b0c5f3d5a00cba2ce3baa994069acc0ac11afa
Jordan Kergoat <jordan.kergoat@gmail.com>
2015-07-03 14:36:25 +0200

    no message

--- PortalAdmin/views.py
+++ PortalAdmin/views.py
@@ -1,15 +1,39 @@
 
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, FormView, UpdateView
 from braces.views import LoginRequiredMixin, SuperuserRequiredMixin, StaffuserRequiredMixin, SelectRelatedMixin
 from django.views.generic.base import TemplateView
 from Portal.models import Game, Userprofile
 from PortalAdmin.forms import UserForm
 
+def test_context(template_name):
 
-class AdminIndexView(LoginRequiredMixin, StaffuserRequiredMixin, TemplateView):
-    template_name = 'Administration/index.html'
+    def decorator(function):
+        def additionnal_context(function):
+
+            def wrapper(**kwargs):
+                TemplateView.template_name = template_name
+                context = {}
+                context['games'] = ['toto', 'titi']
+                return context
+
+            return wrapper
+
+        TemplateView.get_context_data = method_decorator(additionnal_context)(TemplateView.get_context_data)
+        return TemplateView
+
+    return decorator
+
+
+@test_context('Administration/index.html')
+# class AdminIndexView(LoginRequiredMixin, StaffuserRequiredMixin, TemplateView):
+class AdminIndexView(TemplateView):
+    pass
+    # template_name = 'Administration/index.html'
+
+
 
 
 # DEBUT MEMBRES
